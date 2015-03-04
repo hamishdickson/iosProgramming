85029da7141a3d3a7e980f6e6911b0018f029c2e
mishyTheBear <hamish.dickson@gmail.com>
2015-03-04 08:32:22 +0000

    add pickers

---
+++ Picker/Picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picker
+//
+//  Created by Hamish Dickson on 04/03/2015.
+//  Copyright (c) 2015 HamishDickson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
