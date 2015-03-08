c7d4ce41a19e2c1ec94a6312d0c829f43fd3a69b
mishyTheBear <hamish.dickson@gmail.com>
2015-03-08 17:05:49 +0000

    add picker content

--- Picker/Picker/SingleComponentPickerViewController.swift
+++ Picker/Picker/SingleComponentPickerViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var singlePicker: UIPickerView!
+    
+    private let characterNames = ["Luke", "Han", "Leia", "Chewbacca", "Artoo", "Threepio", "Lando"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,5 +37,28 @@ class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegat
     */
 
     @IBAction func buttonPressed(sender: AnyObject) {
+        let row = singlePicker.selectedRowInComponent(0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)"
+        
+        let alert = UIAlertController(title: title, message: "Thank you for choosing", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "You're welcome", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+
+    // MARK:-
+    // MARK: Picker Data Source Methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return characterNames.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return characterNames[row]
     }
 }
