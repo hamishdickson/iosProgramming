a79064a5751f0d1b2c2c59273383a8519a11da3c
hamishdickson <hamish.dickson@gmail.com>
2015-03-18 09:35:37 +0000

    add dependent picker guts

--- Picker/Picker/DependentComponentPickerViewController.swift
+++ Picker/Picker/DependentComponentPickerViewController.swift
@@ -22,6 +22,15 @@ class DependentComponentPickerViewController: UIViewController, UIPickerViewDele
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let bundle = NSBundle.mainBundle()
+        let plistURL = bundle.URLForResource("statedictionary", withExtension: "plist")
+        stateZips = NSDictionary(contentsOfURL: plistURL!) as [String: [String]]
+        
+        let allStates = stateZips.keys
+        states = sorted(allStates)
+        
+        let selectedState = states[0]
+        zips = stateZips[selectedState]
     }
 
     override func didReceiveMemoryWarning() {
@@ -56,5 +65,38 @@ class DependentComponentPickerViewController: UIViewController, UIPickerViewDele
         
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    // MARK:-
+    // MARK: Picker Data Source Marker
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == stateComponent {
+            return states.count
+        } else {
+            return zips.count
+        }
+    }
+    
+    // MARK:-
+    // MARK: Picker Delegate Methods
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        if component == stateComponent {
+            return states[row]
+        } else {
+            return zips[row]
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == stateComponent {
+            let selectedState = states[row]
+            zips = stateZips[selectedState]
+            dependentPicker.reloadComponent(zipComponent)
+            dependentPicker.selectRow(0, inComponent: zipComponent, animated: true)
+        }
     }
 }
