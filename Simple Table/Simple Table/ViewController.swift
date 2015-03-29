321e29679bbe34be88b4dfe1a05120e3089b3776
hamishdickson <hamish.dickson@gmail.com>
2015-03-29 17:13:09 +0100

    nice pretty image

--- Simple Table/Simple Table/ViewController.swift
+++ Simple Table/Simple Table/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    private let dwarves = [
+        "Sleepy", "Sneezy", "Bashful", "Happy",
+        "Doc", "Grumpy", "Dopey",
+        "Thorin", "Dorin", "Nori", "Ori",
+        "Balin", "Dwalin", "Fili", "Kili",
+        "Oin", "Gloin", "Bifur", "Bofur",
+        "Bombur"
+    ]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier) as? UITableViewCell
+        
+        if (cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
+        }
+        
+        let image = UIImage(named: "star")
+        cell!.imageView.image = image
+        let highlightedImage = UIImage(named: "star2")
+        cell!.imageView.highlightedImage = highlightedImage
+        
+        cell!.textLabel.text = dwarves[indexPath.row]
+        return cell!
+    }
 
 }
 
